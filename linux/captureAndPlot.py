658cb00a786a34b9191d6a92e0a6c1b82e16eaa9
JusticeProject <93150828+JusticeProject@users.noreply.github.com>
2025-04-21 17:37:08 -0500

    python updates for quick capture

--- linux/captureAndPlot.py
+++ linux/captureAndPlot.py
@@ -5,8 +5,11 @@
 
 import serial
 import time
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+
+###################################################################################################
 
 def plotData(filename):
     fd = open(filename, "r")
@@ -29,48 +32,68 @@ def plotData(filename):
     plt.axis([0, x[-1], 0, 3.4])
     plt.show()
 
+###################################################################################################
+
+def pwmOn(ser):
+    # 125 MHz / 125 = 1MHz counter
+    ser.write(b"p00125")
+    data = ser.read(6)
+    print(data)
+
+    # 1000 counts / 1MHz = 0.001 sec PWM period (1 kHz)
+    ser.write(b"w01000")
+    data = ser.read(6)
+    print(data)
+
+    # duty cycle is half of the wrap value of 1000
+    ser.write(b"d00500")
+    data = ser.read(6)
+    print(data)
+
+###################################################################################################
+
+def pwmOff(ser):
+    ser.write(b"d00000")
+    data = ser.read(6)
+    print(data)
+
+    ser.write(b"m")
+    data = ser.read(1)
+    print(data)
+
+###################################################################################################
+
+def doCapture(ser):
+    ser.write(b"c")
+    data = ser.read(1)
+    print(data)
+
+    time.sleep(1)
+
+    binData = b""
+    for i in range(0, 2048):
+        ser.write(b"l")
+        binData = binData + ser.read(64)
+        
+    print(f"len(binData) = {len(binData)}")
+    fd = open("data1.txt", "w")
+    for item in binData:
+        fd.write(str(item) + "\n")
+    fd.close()
+
+###################################################################################################
+
+debug = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == "debug":
+        debug = True
+
 ser = serial.Serial("/dev/ttyACM0", 115200)
 
-ser.write(b'z')
-data = ser.read(1)
-print(data)
-
-ser.write(b'p00125')
-data = ser.read(6)
-print(data)
-
-ser.write(b'w60000')
-data = ser.read(6)
-print(data)
-
-ser.write(b'd30000')
-data = ser.read(6)
-print(data)
-
-ser.write(b'c')
-data = ser.read(1)
-print(data)
-
-time.sleep(1)
-
-ser.write(b'd00000')
-data = ser.read(6)
-print(data)
-
-ser.write(b'm')
-data = ser.read(1)
-print(data)
-
-binData = b""
-for i in range(0, 2048):
-    ser.write(b'l')
-    binData = binData + ser.read(64)
-
-print(f"len(binData) = {len(binData)}")
-fd = open("data1.txt", "w")
-for item in binData:
-    fd.write(str(item) + "\n")
-fd.close()
-
+if debug:
+    pwmOn(ser)
+doCapture(ser)
+if debug:
+    pwmOff(ser)
 plotData("data1.txt")
 
